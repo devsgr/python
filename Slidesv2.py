ef666c754e33e234d5f0744641a7964f61928719
Dev Shrestha <33006162+devsgr@users.noreply.github.com>
2024-08-02 11:50:28 -0700

    With restart and web check

--- Slidesv2.py
+++ Slidesv2.py
@@ -15,6 +15,7 @@ import os
 import json
 #Change the following parameters to match your settings
 Unit = 2  #The Kiosk number
+RebootTime = 3 #(3 AM)
 Baseurl = "https://www.webpages.uidaho.edu/iac/BEL336/" #Web site URL where index.html is located
 BaseFolder = "/home/pi/www/" #Path to the base folder. Create this folder in pi. This folder must exist
 Channel = "123456789" # Thing Speak Channel ID
@@ -33,6 +34,7 @@ def ReadField1():
     else:
         j = 'X'
     return j
+
 # WriteField1 Writes the field 1 to Thinkspeak channel. 
 def WriteField1(msg):
 #Write back
@@ -42,6 +44,7 @@ def WriteField1(msg):
          return True
     else:
         return False
+
 # ValueIs2 is a simple function to see if value is 2 for this kiosk to upadate
 def ValueIs2(v):
 # If v at Unit is 2, return true else return false 
@@ -59,12 +62,20 @@ def is_Online(url):
         return True
     except requests.RequestException:
         return False
+
 # upadate is the main function that
 # 1. Reads and save the index.html to local folder 
 # 2. Parse the index file to get all the linked image and video fiels
-# 3. Download and save all image and video files to local folder        
+# 3. Download and save all image and video files to local folder
+
 def update(Baseurl,BaseFolder):
-# read and Copy index.html
+
+    # Open and write the update time to Log.txt
+    fl = open(BaseFolder +'Log.txt','a') #Cant write temp.txt but Temp.txt is fine
+    now = datetime.now()
+    fl.write( now.strftime("%m/%d%Y, %H:%M:%S") +'\n' ) 
+    
+    # read and Copy index.html
     FileName = 'index.html'
     r = requests.get(Baseurl + FileName)
     WritePath = BaseFolder + FileName
@@ -75,31 +86,32 @@ def update(Baseurl,BaseFolder):
                 f.write(chunk) 
     
     f.close()
-   
-    #f.write(r.text)
-    #os.setxattr(WritePath,'Copied',r.headers['Last-Modified']) Trying to use attribute to copy file length did not work 
-    #print(os.getxattr(WritePath,'Copied'))
+    # fl.write( 'Copied: '  + WritePath +'\n') # Optional to write copied file to log file
+    # f.write(r.text)
+    # os.setxattr(WritePath,'Copied',r.headers['Last-Modified']) Trying to use attribute to copy file length did not work 
+    # print(os.getxattr(WritePath,'Copied'))
     x = 0
 
     # Now reading from the index file, copy all image and video files
     while (x >= 0):
         x1 = r.text.find("Slides/",x+1) #returns -1 if not found
         x2 = r.text.find("Videos/",x+1) #returns -1 if not found
-        if (x1 >=0 and x2 >=0): # There are more slides or video get the first one
+        if (x1 >=0 and x2 >=0): # There are more slides or video get the first one, returns -1 when EOF
             x = min(x1,x2)
         else: #There is either no Videos or no Slides returning -1 for that
             x = max(x1,x2)
-        #print(x)
+        # print(x)
         if (x >= 0):
-
+            
+            # Parse text to find the video or image file    
             y = r.text.find('.', x+1)
             FileName = r.text[x:y+4]
             downloadurl = Baseurl + FileName
-            WritePath = BaseFolder + FileName # add .replace('/','\\') for windows
+            WritePath = BaseFolder + FileName
+
             # Videos should be downloaded only if the length doesn't match or the file does not exist
-
             if (r.text[x:x+6]=='Videos' and os.path.exists(WritePath)):
-
+                
                 req = requests.head(downloadurl)
                 url_Size = req.headers['Content-Length']
                 file_Size = os.path.getsize(WritePath)
@@ -117,8 +129,16 @@ def update(Baseurl,BaseFolder):
                         if chunk:
                                 f.write(chunk) 
                 f.close()
-tomorrow_3am = (datetime.now() + timedelta(days=1)).replace(hour=3, minute=0, second=0, microsecond=0)                
+                #fl.write( 'Copied: '  + downloadurl +'\n')
+    now = datetime.now()
+    fl.write( "Updaetd successfully" +'\n')           
+    fl.close()                
+
+# Check to see if internet is connected
+
+NextReboot = (datetime.now() + timedelta(days=1)).replace(hour = RebootTime, minute=0, second=0, microsecond=0)
 pid = subprocess.Popen('chromium-browser --kiosk ~/www/CssJs/Updating.html &', shell = True)
+
 # Try and wait 10 seconds up to 10 times to come the unit online, if fails load old page
 TryCount = 0
 while (not is_Online(Baseurl) and TryCount<10):
@@ -127,15 +147,22 @@ while (not is_Online(Baseurl) and TryCount<10):
 
 if (TryCount < 10):
     update(Baseurl,BaseFolder)
+
+Reload = True  
 pid.terminate()
-Reload = True
+#print("pid terminated")
+
 while True:
     # Run the index file from the local www path
     if(Reload):
         pid = subprocess.Popen('chromium-browser --kiosk ~/www/index.html &', shell = True)
     Reload = False
     Now = datetime.now()
+    if(Now >= NextReboot):
+        os.system('sudo reboot')
+
     NextRun = Now + timedelta(minutes=UpdateInterval)
+    
     SleepSec = (NextRun - Now).total_seconds()
     sleep(SleepSec)
     # 1. Check Thingspeak field 1
@@ -145,15 +172,13 @@ while True:
     # Make sure in Thingspeak that the length of v is greater than the unit value
     if(ValueIs2(v)):
         # Switch to updating message
-        pid.terminate()
         pid = subprocess.Popen('chromium-browser --kiosk ~/www/CssJs/Updating.html &', shell = True)
-        update(Baseurl,BaseFolder)
-        pid.terminate()
+        update(Baseurl,BaseFolder) 
         Reload = True
     # Write is active
     
     if (v[Unit-1] != "0"): # Write 0 only if the value is not already 0 (Avoids unnecessary writing as ThingSpeak has 15 seconds writing limit
         msg = v[:Unit-1] + "0" + v[Unit:] # In Python index starts at 0 and in thing speak, the first number belongs to Unit 1
         while (not WriteField1(msg)):
-            sleep(16) #Thing Speak has 15 seconds interval limit. Try again after 16 seconds (1 second buffer)
-
+            random_number = random.randint(1, 100)
+            sleep(16+random_number) #Thing Speak has 15 seconds interval limit. Try again after 16+ random seconds (1 second buffer)
